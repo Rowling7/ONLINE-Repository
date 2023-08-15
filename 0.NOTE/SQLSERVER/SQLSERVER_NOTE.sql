7982c594fe00cf22e5c5aaf564f0abc01e23ea36
ysongcao <2804119807@qq.com>
2023-08-15 15:20:08 +0800

    整理了全部SQL脚本

--- 0.NOTE/SQLSERVER/SQLSERVER_NOTE.sql
+++ 0.NOTE/SQLSERVER/SQLSERVER_NOTE.sql
@@ -7,7 +7,17 @@
 SELECT RANK()  OVER( PARTITION BY UID ORDER BY AGE DESC ) ,UID,AGE,ID
 FROM INFO
 
-
+--审核用
+--判空
+	where nullif(a0103,'') is null
+--where 条件
+	where AND A0102 LIKE '#A0102#%' AND A0102 LIKE '#GLDW#%'
+--国家高速公路、省道高速公路（无匝道）
+	--k0304技术等级-10：高速
+	WHERE len(ltrim(rtrim(k0101)))<=9 and k0304='10'
+--纳入里程统计
+	--K0123不是断头路，K0124没有重复路段
+	where isnull(k0123,'2')='2' and nullif(k0124,'') is null
 
 
 --<------------------------------<---SQL 0.1.1--->------------------------------>--
--- 2.SQL 脚本/SQLSERVER/SQL_按行政区划分路线里程.sql
+++ 2.SQL 脚本/SQLSERVER/SQL_按行政区划分路线里程.sql
@@ -1,3 +1,30 @@
+--各市区总计G
+select
+case when left(A0103 ,4) = '4290' then left(A0103 ,6) else left(A0103 ,4) end,
+sum(CASE WHEN isnull(K0124,'') = '' and isnull(K0123,'2') = '2' and  isnull(K0304,0)<>'10'THEN isnull(K0114,0) else 0 END) '里程',
+sum(CASE WHEN isnull(K0124,'') = '' and isnull(K0304,0)='11' and isnull(K0123,'2') = '2' THEN K0114 else 0 END) '一级',
+sum(CASE WHEN isnull(K0124,'') = '' and isnull(K0304,0)='22' and isnull(K0123,'2') = '2' THEN K0114 else 0 END) '二级',
+sum(CASE WHEN isnull(K0124,'') = '' and isnull(K0304,0)='23' or  isnull(K0304,0)='24' or isnull(K0304,0)='30' and isnull(K0123,'2') = '2' THEN K0114 else 0 END) '二级以下'
+from K01
+where left(k0101,1) in ('G')
+group by case when left(A0103 ,4) = '4290' then left(A0103 ,6) else left(A0103 ,4) end with rollup
+order by case when left(A0103 ,4) = '4290' then left(A0103 ,6) else left(A0103 ,4) end
+
+
+--各市区总计S
+select
+case when left(A0103 ,4) = '4290' then left(A0103 ,6) else left(A0103 ,4) end,
+sum(CASE WHEN isnull(K0124,'') = '' and isnull(K0123,'2') = '2' and  isnull(K0304,0)<>'10'THEN isnull(K0114,0) else 0 END) '里程',
+sum(CASE WHEN isnull(K0124,'') = '' and isnull(K0304,0)='11' and isnull(K0123,'2') = '2' THEN K0114 else 0 END) '一级',
+sum(CASE WHEN isnull(K0124,'') = '' and isnull(K0304,0)='22' and isnull(K0123,'2') = '2' THEN K0114 else 0 END) '二级',
+sum(CASE WHEN isnull(K0124,'') = '' and (isnull(K0304,0)='23' or  isnull(K0304,0)='24' or isnull(K0304,0)='30' )and isnull(K0123,'2') = '2' THEN K0114 else 0 END) '二级以下'
+from K01
+where left(k0101,1) in ('S')
+group by case when left(A0103 ,4) = '4290' then left(A0103 ,6) else left(A0103 ,4) end with rollup
+order by case when left(A0103 ,4) = '4290' then left(A0103 ,6) else left(A0103 ,4) end
+
+
+
 --各市区总计
 select
 case when left(A0103 ,4) = '4290' then left(A0103 ,6) else left(A0103 ,4) end,
@@ -37,7 +64,6 @@ sum(case when left(k0101,1)='X' then isnull(k0114,0) end)	县道,
 sum(case when left(k0101,1)='Y' then isnull(k0114,0) end)	乡道,
 sum(case when left(k0101,1)='C' then isnull(k0114,0) end)	村道,
 sum(case when left(k0101,1)='Z' then isnull(k0114,0) end)	专用公路
-
 from K01
 where left(k0101,1) in ('G','S','X','Y','C','Z')
 and  isnull(K0124,'') = '' and isnull(K0123,'2') = '2' and isnull(K0304,0)<>'10'
