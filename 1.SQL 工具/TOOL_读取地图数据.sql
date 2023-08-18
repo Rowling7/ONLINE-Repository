a50736cd4505ccb921991a9fd7c8a135e6f4c304
ysongcao <2804119807@qq.com>
2023-08-18 17:32:03 +0800

    更新读取地图sql

--- 1.SQL 工具/TOOL_读取地图数据.sql
+++ 1.SQL 工具/TOOL_读取地图数据.sql
@@ -1,4 +1,12 @@
+--Microsoft.Jet.OLEDB.4.0
 select distinct rtrim(DWDM)
 from
-openrowset('Microsoft.Jet.OLEDB.4.0',  'D:\东方永德软件OYDSOFT\HRP-GIS2015\MAPDATA\42\mapdata.sdd';'admin';'',QL)
+openrowset('Microsoft.Jet.OLEDB.4.0',  'E:\42\mapdata.sdd';'admin';'',QL)
+order by  rtrim(DWDM)
+
+
+--Microsoft.ACE.OLEDB.12.0
+select distinct rtrim(DWDM)
+from
+openrowset('Microsoft.ACE.OLEDB.12.0', 'E:\42\mapdata.sdd';'admin';'',QL)
 order by  rtrim(DWDM)
