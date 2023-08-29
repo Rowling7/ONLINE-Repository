b462a9d24217386a4c4a704cb51c8ddfee2a6e65
ysongcao <2804119807@qq.com>
2023-08-29 10:59:02 +0800

    更新START和新增临时文件

--- 3.Others/TOOLS/Start.bat
+++ 3.Others/TOOLS/Start.bat
@@ -20,13 +20,10 @@ start D:\"Program Files (x86)"\WXWork\WXWork.exe
 :: start C:\"Program Files (x86)"\"Microsoft SQL Server"\90\Tools\Binn\VSShell\Common7\IDE\Ssms.exe
 
 :: Edge
-::  start C:\"Program Files (x86)"\Microsoft\Edge\Application\msedge.exe
+:: start C:\"Program Files (x86)"\Microsoft\Edge\Application\msedge.exe
 
 :: 此电脑
 :: start C:\Users\OYDSOFT\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\"System Tools"\computer.lnk
-
-:: 关闭todesk
-taskkill /f /im ToDesk.exe
 
 :: 关闭向日葵
 taskkill /f /im SunloginClient.exe
@@ -38,6 +35,9 @@ sc stop "ToDesk_Service"
 
 :: 关闭edge 后台
 taskkill /f /im MSEDGE.EXE
+
+:: 关闭Chrome 后台
+taskkill /f /im Chrome.EXE
 
 pause
 
