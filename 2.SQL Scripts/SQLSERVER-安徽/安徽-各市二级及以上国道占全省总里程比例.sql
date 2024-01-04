b86c102de426a2af46125bdd0b7ed5ea1b5327d7
ysongcao <2804119807@qq.com>
2024-01-04 15:26:44 +0800

    更新安徽部分脚本

--- 2.SQL Scripts/SQLSERVER-安徽/安徽-各市二级及以上国道占全省总里程比例.sql
+++ 2.SQL Scripts/SQLSERVER-安徽/安徽-各市二级及以上国道占全省总里程比例.sql
@@ -1,13 +1,13 @@
 SELECT  --广德合进宣城 ，宿松合进安庆
 replace(REPLACE(left(STUFF(rtrim(a0102),3,1,null),4), '3422', '3418'),'3421','3408') a0102,objjc,
 sum(CASE WHEN left(k0101,1)='g'and  k0304 in ('11','22') and len(rtrim(k0101))>9 THEN isnull(k0114,'')	ELSE 0 END ) '二级及以上'
-from k01 
+from k01
 left join (
-select 
+select
 STUFF(rtrim(objname),3,1,null) objname,left(objjc,3) objjc
 from DA0102
 where len(rtrim(objname))=5 AND  OBJJC LIKE '%干线公路%' )DA0102 on replace(REPLACE(left(STUFF(rtrim(a0102),3,1,null),4), '3422', '3418'),'3421','3408')=rtrim(objname)
-where a0102 like '341%'  
+where a0102 like '341%'
 			and isnull(k0123,'2')='2' and k0124 is null
 group by replace(REPLACE(left(STUFF(rtrim(a0102),3,1,null),4), '3422', '3418'),'3421','3408'),objjc
 ORDER BY a0102
---
+++ 2.SQL Scripts/SQLSERVER-安徽/汇编用到的脚本/各地市2023年新增一级路二级路里程.sql
@@ -0,0 +1,13 @@
+--各地市2023年新增一级路二级路里程
+SELECT LEFT(STUFF(rtrim(A0102),3,1,null),4),SUM(ISNULL(A0307,0)),SUM(ISNULL(A0308,0))
+from A11
+WHERE SUBSTRING(A11.A0101,1,4) =   '2023'
+AND A11.A3219 =   '1' AND LEFT(STUFF(rtrim(A0102),3,1,null),4)IN(
+select --查询市州
+STUFF(rtrim(objname),3,1,null) objjc
+from DA0102
+where len(rtrim(objname))=5 AND  OBJJC LIKE '%干线公路%' 
+)
+--and LEFT(rtrim(A0102),5) ='34201'
+GROUP BY LEFT(STUFF(rtrim(A0102),3,1,null),4)
+
