3e74ed1bd9f194a58364d79aaae4493ae57f88c2
ysongcao <2804119807@qq.com>
2024-02-07 11:28:53 +0800

    更新脚本

--- 1.TOOLS/1.SQL 工具/TOOL_查询表字段.sql
+++ 1.TOOLS/1.SQL 工具/TOOL_查询表字段.sql
@@ -1,4 +1,4 @@
-select 
+select
 	c1.fieldname zdcode1,
 	case when c2.ftype = 'd' then 'h'+c1.fieldname else null end zdcode2,
 	c2.objjc,
@@ -9,6 +9,6 @@ select
 from oydst_fieldcont1 as p
 join oydst_fieldcont1 as c1 	on p.objid = c1.parentid
 join oydst_fields 		as c2		on c1.fieldname = c2.objname
-where c1.fieldname is not null 
+where c1.fieldname is not null
 			and p.tablename = 'K063'
-order by p.tablename,c1.fieldname
+order by p.tablename,c1.fieldnameD
